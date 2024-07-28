c13dd4d3242ab5b0f1744384d93eec594b21c224
AMAN KUMAR VERMA <vermaaman714600@gmail.com>
2024-07-28 16:15:47 +0530

    Update locationRoutes.js

--- Backend/routes/locationRoutes.js
+++ Backend/routes/locationRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { getLocations,saveLocation,deleteLocation } = require('../controllers/locationController');
 const router = express.Router();
-router.post('/',saveLocation);
-router.get('/', getLocations);
-router.delete('/:id',deleteLocation);
+router.post('/api/users',saveLocation);
+router.get('/api/locations', getLocations);
+// router.delete('/:id',deleteLocation);
 
 module.exports = router;
