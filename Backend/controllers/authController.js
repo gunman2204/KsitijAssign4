2d4ad16fb45b639f46292d9134cb37afe3af8c7c
AMAN KUMAR VERMA <vermaaman714600@gmail.com>
2024-07-31 23:24:35 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,58 +1,48 @@
-const User=require('../models/User')
-const jwt =require('jsonwebtoken');
-const dotenv=require('dotenv');
+const User = require('../models/User')
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
 dotenv.config();
-const generateToken=(id)=>{
-    return jwt.sign({id},process.env.JWT_secret,{
-        expiresIn:'30d',
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_secret, {
+        expiresIn: '30d',
     });
 };
 
-exports.registerUser = async (req,res) => {
-    const { username,password}=req.body;
-    try{
-        // const userExists=await User.findOne({username})
-        // if(userExists){
-        //     return res.status(400).json({message:'User already Exists'});
-
-        // }
-        const user=await User.create({
+exports.registerUser = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        
+        const user = await User.create({
             username,
             password,
         });
-        // if(user){
-        //     res.status(201).json({
-        //         _id:user._id,
-        //         username: user.username,
-        //     });
-        // }
-        // else{
-        //     res.status(400).json({message:'Invalid user data'});
-        // }
+        console.log(user)
+        console.log('registerUser success at backend')
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+        })
+        
     }
-    catch(err){
-        res.status(500).json({message:err.message});
+    catch (err) {
+        res.status(500).json({ message: err.message });
     }
 }
-exports.loginUser=async(req,res)=>{
+exports.loginUser = async (req, res) => {
     console.log(req.body);
-    const {username,password}=req.body;
-    try{
-        const user=    await User.findOne({username});
-    if(user || (await user.matchPassword(password))){
-        // res.json({
-        //     _id:user._id,
-        //     username:user.username,
-        //     token:generateToken(user._id)
-        // });
-        console.log('Logged In')
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username, password });
+        if (user) {
+            res.status(200).json({ message: 'User Found', userId: user.id })
+        }
+        else {
+            res.status(401).json({ message: 'User Not Found' })
+        }
     }
-    else{
-        res.status(401).json({message:"Invalid credentials"})
-    }
-}
-    catch(err){
-        res.status(500).json({message:err.message});
+    catch (err) {
+        res.status(500).json({ message: err.message });
     }
 }
+
